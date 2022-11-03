855256bcd65adf2aa514944da000b064a40a1561
topit <82128920+topitbopit@users.noreply.github.com>
2022-11-02 22:13:12 -0700

    Version 1.2

--- PlayerRadar/loadstring.lua
+++ PlayerRadar/loadstring.lua
@@ -1,34 +1,39 @@
---- Player radar
---- Made with the drawing library
+--- Drawing Player Radar
+--- Made by topit
 
 _G.RadarSettings = {
     -- Radar settings
     RADAR_LINES = true; -- Displays distance rings
-    SMOOTH_ROT = true; -- Rotates the radar smoothly
     RADAR_SCALE = 1; -- Controls how "zoomed in" the radar display is 
-    RADAR_RADIUS = 100; -- The size of the radar itself
+    RADAR_RADIUS = 125; -- The size of the radar itself
     RADAR_LINE_DISTANCE = 50; -- The distance between each line
-    
+    RADAR_ROTATION = true; -- Toggles radar rotation. Looks kinda trippy when disabled
+    SMOOTH_ROT = true; -- Rotates the radar smoothly
+    SMOOTH_ROT_AMNT = 20; -- Lower number is smoother, higher number is snappier 
+    CARDINAL_DISPLAY = true; -- Displays each cardinal direction (NSWE) around the radar 
+
     -- Marker settings
+    USE_QUADS = true; -- Displays radar markers as arrows instead of dots 
+    OFFSCREEN_TRANSPARENCY = 0.3; -- Transparency of offscreen markers
     DISPLAY_TEAM_COLORS = true; -- Sets the radar markers' color to their player's team color
     DISPLAY_OFFSCREEN = true; -- Leaves offscreen markers visible
     DISPLAY_TEAMMATES = true; -- Shows your teammates' markers
-    OFFSCREEN_TRANSPARENCY = 0.4; -- Transparency of offscreen markers
-    USE_QUADS = true; -- Makes radar markers quads instead of circles
-    MARKER_SCALEMIN = 0.85; -- Minimium scale radar markers can be
-    MARKER_SCALEMAX = 1.75; -- Maximum scale radar markers can be 
+    MARKER_SCALEMIN = 0.75; -- Minimium scale radar markers can be. Marker falloff bypasses this limit!
+    MARKER_SCALEMAX = 1.75; -- Maximum scale radar markers can be. Marker falloff bypasses this limit!
+    MARKER_FALLOFF = false; -- Affects the markers' scale depending on how far away the player is
+    MARKER_FALLOFF_AMNT = 500; -- How close someone has to be for falloff to start affecting them 
 
     -- Theme
     RADAR_THEME = {
-        Outline = Color3.fromRGB(6, 6, 6);
-        Background = Color3.fromRGB(25, 25, 30);
-        DragHandle = Color3.fromRGB(51, 51, 255);
+        Outline = Color3.fromRGB(35, 35, 45); -- Radar outline
+        Background = Color3.fromRGB(25, 25, 35); -- Radar background
+        DragHandle = Color3.fromRGB(50, 50, 255); -- Drag handle 
         
-        Cardinal_Lines = Color3.fromRGB(100, 100, 155); -- Color of the horizontal and vertical lines
-        Distance_Lines = Color3.fromRGB(64, 64, 69); -- Color of the distance rings
+        Cardinal_Lines = Color3.fromRGB(110, 110, 120); -- Color of the horizontal and vertical lines
+        Distance_Lines = Color3.fromRGB(65, 65, 75); -- Color of the distance rings
         
-        Generic_Marker = Color3.fromRGB(255, 0, 107); -- Color of a player marker without a team
-        Local_Marker = Color3.fromRGB(16, 235, 255); -- Color of your marker, regardless of team
+        Generic_Marker = Color3.fromRGB(255, 25, 115); -- Color of a player marker without a team
+        Local_Marker = Color3.fromRGB(115, 25, 255); -- Color of your marker, regardless of team
     };
 }
 
